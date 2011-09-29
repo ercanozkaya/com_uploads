20fa61d8ec516a5831c32fc7e80f0b0374d35031
Amy Stephen <AmyStephen@gmail.org>
2011-09-29 14:38:28 -0500

    Edited administrator/components/com_uploads/install/install.sql via GitHub

--- administrator/components/com_uploads/install/install.sql
+++ administrator/components/com_uploads/install/install.sql
@@ -10,6 +10,6 @@ KFactory::get('com://admin/files.controller.container')->add(array(
 	)
 )); */
 
-INSERT INTO `jos_files_containers` (`files_container_id`, `slug`, `title`, `path`, `parameters`)
+INSERT INTO `#_files_containers` (`files_container_id`, `slug`, `title`, `path`, `parameters`)
 VALUES
 	(NULL, 'uploads-manage', 'Demo Container for Uploads', 'images', '{\"upload_extensions\":\"jpg,jpeg,png,gif,pdf,txt\",\"upload_maxsize\":10485760,\"check_mime\":0}');
